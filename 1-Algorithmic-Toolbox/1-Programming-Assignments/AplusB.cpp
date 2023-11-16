5237a341ac844ebab96862af9bc3e405cc219bd8
Mahi Nuthanapati <mahidhar1287@gmail.com>
2023-11-16 10:34:34 -0500

    completed assignment

---
+++ 1-Algorithmic-Toolbox/1-Programming-Assignments/AplusB.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std ;
+
+int main( ) {
+    int a = 0 ;
+    int b = 0 ; 
+    int sum = 0 ;
+    cin >> a >> b ;
+    sum = a + b ;
+    cout << sum ;
+    return 0 ;
+}
